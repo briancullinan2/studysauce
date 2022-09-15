491420c9ca2bbe77d6503a4450bbd327b05e2765
megamind <megamindbrian@gmail.com>
2022-09-15 16:40:13 -0700

    closer to file uploader

--- docs/sauce.js
+++ docs/sauce.js
@@ -1,6 +1,8 @@
 
 const CHUNK_SIZE = 1000
 
+const SAVE_URL = '/fileput'
+
 const BACKGROUND_NEURAL = [
   './neural_brain.jpeg',
   './neural_darkened.jpg',
@@ -10,7 +12,7 @@ const BACKGROUND_NEURAL = [
 
 const files = []
 
-function uploadFiles(ev) {
+async function uploadFiles(ev) {
   if (ev.dataTransfer.items) {
     // Use DataTransferItemList interface to access the file(s)
     [...ev.dataTransfer.items].forEach((item, i) => {
@@ -24,42 +26,73 @@ function uploadFiles(ev) {
       files.push(file)
     });
   }
+
   // TODO: why is this even necessary? why in the hell would someone ever design 2 separate interfaces for a drag and drop dataTransfer? probably because someone didn't plan ahead the first time, and someone else decided they couldn't break something else. which one is it? .items or .files? MORE CODE is obviously the only answer
   // TODO: upload multiple files?
-  debugger
+  if(!files.length) {
+    return
+  }
+  const currentFile = files[0]
+  const chunks = currentFile.size / CHUNK_SIZE
+  const fileData = new Uint8Array(await currentFile.arrayBuffer())
+
+  for(let i = 0; i < Math.ceil(chunks); i++) {
+    let segmentData = fileData.slice(i * CHUNK_SIZE, i * CHUNK_SIZE + CHUNK_SIZE)
+    debugger
+    await fetch(SAVE_URL, {
+      method: 'PUT',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        part: i,
+        data: Array.from(segmentData)
+      })
+    })
+  }
 }
 
-window.addEventListener('load', function () {
-  let dropzone = document.getElementById('drop_zone')
+if(typeof window != 'undefined') {
+  document.body.style.backgroundImage = 'url(' 
+    + BACKGROUND_NEURAL[Math.floor(Math.random() * BACKGROUND_NEURAL.length)] + ')'
 
-  dropzone.addEventListener('dragenter', function () {
-    dropzone.classList.add('dragging')
+
+  window.addEventListener('load', function () {
+    let dropzone = document.getElementById('drop_zone')
+
+    dropzone.addEventListener('dragenter', function () {
+      dropzone.classList.add('dragging')
+    })
+
+    dropzone.addEventListener('dragleave', function () {
+      dropzone.classList.remove('dragging')
+    })
+
+    dropzone.addEventListener('dragover', function (ev) {
+      ev.preventDefault()
+    })
+
+    // TODO: not sure when this files but this doesn't appear to work
+    //   only works on an unsuccessful drap/drop? only works at window level? ambiguity everywhere
+    /*
+    dropzone.addEventListener('dragend', function (ev) {
+      ev.preventDefault()
+      dropzone.classList.remove('dragging')
+    })
+    */
+
+    dropzone.addEventListener("drop",function (ev) {
+      ev.preventDefault()
+      dropzone.classList.remove('dragging')
+      uploadFiles(ev)
+    })
+
   })
 
-  dropzone.addEventListener('dragleave', function () {
-    dropzone.classList.remove('dragging')
-  })
+}
 
-  dropzone.addEventListener('dragover', function (ev) {
-    ev.preventDefault()
-  })
-
-  // TODO: not sure when this files but this doesn't appear to work
-  //   only works on an unsuccessful drap/drop? only works at window level? ambiguity everywhere
-  /*
-
-
-  dropzone.addEventListener('dragend', function (ev) {
-    ev.preventDefault()
-    dropzone.classList.remove('dragging')
-  })
-  */
-
-  dropzone.addEventListener("drop",function (ev) {
-    ev.preventDefault()
-    dropzone.classList.remove('dragging')
-    uploadFiles(ev)
-  })
-
-})
-
+if(typeof module != 'undefined') {
+  module.exports = {
+    SAVE_URL,
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const { SAVE_URL } = require('./docs/sauce.js')
 
 const INDEX = fs.readFileSync('./index.html').toString('utf-8')
 const NON_ALPHA_NUM = (/[^a-z0-9-_]/gi)
@@ -32,9 +35,55 @@ function renderIndex() {
 	fs.writeFileSync(path.join(__dirname, 'docs/index.html'), index)
 }
 
-
-
-module.exports = {
-	renderIndex
+function randomKey(length = 8) {
+  return Math.random().toString(16).substr(2, length);
 }
 
+function uploadFile(req, res, next) {
+  console.log(req.body)
+  // TODO: accept file uploads as minimal example, this took long because the machine is working against me
+
+  return next()
+}
+
+function loadContent(req, res, next) {
+  let pathname = req.originalUrl
+  if(pathname == '/') {
+    pathname = '/index.html'
+  }
+  const filename = path.join(__dirname, '/docs/', req.originalUrl)
+  if(pathname == '/index.html'
+    && !fs.existsSync(filename)) {
+    renderIndex()
+  }
+  if(fs.existsSync(filename)) {
+    return res.sendFile(filename)
+  }
+  return next()
+}
+
+// TODO: PUT 405 error from NPM's `live-server`, no workaround listed, too complicated, not worth fighting about or fixing, good for simply static pages, not good for developing a server-side function
+
+
+function startServer() {
+  const app = express()
+  app.enable('etag')
+  app.set('etag', 'weak')
+  app.use(express.json())
+  app.use(loadContent)
+  app.put(SAVE_URL, uploadFile)
+
+  // TODO: nuance, none of these options work anymore, everything is broken, npm is infected or too much complexity to keep track of? why do so many tutorials have bad instructions? I must be living in some alternate reality of hell, https://stackoverflow.com/questions/10867052/cannot-serve-static-files-with-express-routing-and-no-trailing-slash
+  
+  //app.use(express.static(path.resolve(__dirname, './docs/')))
+  //app.all('*', function(req, res) { res.redirect('/main/'); });
+  //app.use('/', express.static(__dirname+'/docs'));
+
+  const { createServer } = require('http')
+  const httpServer = createServer(app).listen(8080)
+  
+}
+
+module.exports = {
+  startServer
+}
